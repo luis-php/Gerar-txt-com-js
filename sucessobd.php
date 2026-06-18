264fe30c909c56e0b1baf5ecbadf7dd5445e3824
Luis <luis.macedo.oliveira@escola.pr.gov.br>
2026-06-18 11:00:36 -0300

    kakaka

---
+++ sucessobd.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Sucesso banco de dados</title>
+</head>
+<body>
+
+    <h2>Sucesso ao gravar banco de dados MySQL!</h2>
+
+    <a href="index.php" title="Ir para página inicial">
+        <img src="src/sucessobd.jpeg" alt="index.php" width="600" height="500">
+    </a>
+</body>
+</html>
