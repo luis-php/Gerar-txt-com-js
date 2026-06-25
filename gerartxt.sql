ab8477b9a14fc21edfb77f569715046ec0bdd17a
Luis <luis.macedo.oliveira@escola.pr.gov.br>
2026-06-25 10:54:56 -0300

    kakak

--- gerartxt.sql
+++ gerartxt.sql
@@ -1,1 +1,12 @@
-SELECT * FROM gerartxt.usuario;
+create database if not exists gerartxt;
+
+use gerartxt;
+
+create table if not exists usuario(
+	nome varchar(50),
+    email varchar(50),
+    senha  varchar(50),
+    nascimento varchar(50),
+    genero varchar(50),
+    texto varchar(50)
+);
