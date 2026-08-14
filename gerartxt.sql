101df3d7bf8bf42143e8f9ec4b4ab796659b8fdd
luis <luis.macedo.oliveira@escola.pr.gov.br>
2026-08-14 09:01:06 -0300

    dadada

--- gerartxt.sql
+++ gerartxt.sql
@@ -1,12 +1,1 @@
-create database if not exists gerartxt;
-
-use gerartxt;
-
-create table if not exists usuario(
-	nome varchar(50),
-    email varchar(50),
-    senha  varchar(50),
-    nascimento varchar(50),
-    genero varchar(50),
-    texto varchar(50)
-);
+SELECT * FROM gerartxt.usuario;
